f5834b87d50fcbe5eff780a97f7d69120ddea583
hotonMo1 <halharithi@waredat.sa>
2023-03-30 05:14:35 +0300

    Initial Commit

---
+++ Aljerah/AljerahApp.swift
@@ -0,0 +1,17 @@
+//
+//  AljerahApp.swift
+//  Aljerah
+//
+//  Created by hoton on 08/09/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct AljerahApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
